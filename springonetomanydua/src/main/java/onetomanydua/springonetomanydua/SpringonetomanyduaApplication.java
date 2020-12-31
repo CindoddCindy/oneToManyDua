f80c3d61aefa6c2f2b2ddec796b5330b8e7f9ea2
CindoddCindy <cindodcindy@gmail.com>
2020-12-31 21:05:54 +0700

    tambah package folder

---
+++ springonetomanydua/src/main/java/onetomanydua/springonetomanydua/SpringonetomanyduaApplication.java
@@ -0,0 +1,13 @@
+package onetomanydua.springonetomanydua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringonetomanyduaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringonetomanyduaApplication.class, args);
+	}
+
+}
---
+++ springonetomanydua/src/test/java/onetomanydua/springonetomanydua/SpringonetomanyduaApplicationTests.java
@@ -0,0 +1,13 @@
+package onetomanydua.springonetomanydua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringonetomanyduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
