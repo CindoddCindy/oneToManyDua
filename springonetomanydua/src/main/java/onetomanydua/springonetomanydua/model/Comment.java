d6640e9d6c477f4e4ddd9d320626e2d391f3f868
CindoddCindy <cindodcindy@gmail.com>
2021-01-01 00:06:13 +0700

    ganti kelas

---
+++ springonetomanydua/src/main/java/onetomanydua/springonetomanydua/model/Comment.java
@@ -0,0 +1,57 @@
+package onetomanydua.springonetomanydua.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends AuditModel  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Lob
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Post post;
+
+    public Comment(@NotNull String text, Post post) {
+        this.text = text;
+        this.post = post;
+    }
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
---
+++ springonetomanydua/src/main/java/onetomanydua/springonetomanydua/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package onetomanydua.springonetomanydua.repository;
+
+
+import onetomanydua.springonetomanydua.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository  extends JpaRepository<Post, Long> {
+}
