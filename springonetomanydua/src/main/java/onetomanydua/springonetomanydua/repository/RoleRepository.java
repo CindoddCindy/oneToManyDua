3040994bea77b3a07d8bb2831850fd93b4fc156c
CindoddCindy <cindodcindy@gmail.com>
2020-12-31 22:15:12 +0700

    tambah kelas kelas

---
+++ springonetomanydua/src/main/java/onetomanydua/springonetomanydua/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package onetomanydua.springonetomanydua.repository;
+
+import onetomanydua.springonetomanydua.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
+}
