f054d3564853219774987ffc3aa794b5f97d419c
CindoddCindy <cindodcindy@gmail.com>
2021-01-01 15:58:37 +0700

    sudah bisa

--- springonetomanydua/src/main/java/onetomanydua/springonetomanydua/SpringonetomanyduaApplication.java
+++ springonetomanydua/src/main/java/onetomanydua/springonetomanydua/SpringonetomanyduaApplication.java
@@ -2,8 +2,10 @@ package onetomanydua.springonetomanydua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringonetomanyduaApplication {
 
 	public static void main(String[] args) {
